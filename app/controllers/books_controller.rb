993e7301d9651d434b00550ccb05952aba053fa0
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2022-09-17 09:27:53 +0000

    [Update]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+
+
+  def index
+    @books = Book.all 
+  end
+
+
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+
+
+
+  def show
+  end
+
+
+
+
+  def edit
+  end
+
+
+
+
+  private
+  # ストロングパラメータ
+  # 下は、book_paramsを定義している。また、これは最後に追加している。
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
+end
